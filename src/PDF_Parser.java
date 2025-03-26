d7aa205369962d090726a100d3e65176e3ddb61a
Amara Gitomer <59937361+Akane-Nishiwake@users.noreply.github.com>
2025-03-26 00:14:00 -0400

    cleaned up unused imports

--- src/PDF_Parser.java
+++ src/PDF_Parser.java
@@ -4,11 +4,9 @@ import com.google.gson.JsonParser;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.text.Paragraph;
-
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 
 public class PDF_Parser {
 

e10e547b54a64e8859488df14e5efe05c7407b24
Amara Gitomer <59937361+Akane-Nishiwake@users.noreply.github.com>
2025-03-26 11:12:46 -0400

    Got Menu Bar in with only exit functionality Cleaned up init() to only be 2 method calls at the moment.

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,6 +4,11 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame {
 
@@ -17,6 +22,10 @@ public class MyFrame extends JFrame {
     private JList inputFileList;
     private JList outputFileList;
     private JButton AddFile;
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenu editMenu;
+    private JMenu settingsMenu;
 
 
     public MyFrame() {
@@ -25,12 +34,55 @@ public class MyFrame extends JFrame {
     }
 
     private void init() {
+        createPanel();
+        setMenuBar();
+
+    }
+    private void createPanel()
+    {
         setTitle("Skill Tree Creator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setContentPane(panel1);
         pack();
         setLocationRelativeTo(null);
+    }
+    private void setMenuBar()
+    {
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        editMenu = new JMenu("Edit");
+        settingsMenu = new JMenu("Settings");
 
+        setFileMenu();
+        setSettingsMenu();
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(settingsMenu);
+        this.setJMenuBar(menuBar);
+    }
+    private void setFileMenu()
+    {
+        JMenuItem newItem = new JMenuItem("New");
+        JMenuItem openItem = new JMenuItem("Open");
+        JMenuItem saveItem = new JMenuItem("Save");
+        JMenuItem exitItem = new JMenuItem("Exit");
+        fileMenu.add(newItem);
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+        exitItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    private void setSettingsMenu ()
+    {
+        JMenuItem preferencesItem = new JMenuItem("Preferences");
+        settingsMenu.add(preferencesItem);
     }
 
 }
